3029f6f9b89fbefe10018298aad15e90d314f98e
sm.assm@gmail.com <hellogit123>
2015-12-20 21:02:12 +0500

    Explained

--- CropImages/App_Start/RouteConfig.cs
+++ CropImages/App_Start/RouteConfig.cs
@@ -13,6 +13,13 @@ namespace CropImages
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+            //register the rout for handler with a URL Pattern like http:/localhost/cropImages/x-y-h-w-image.jpg
+            //so all the similar url Request are processed by Our Generic Handler
+            //our generic handler will parse x as Left , y as Top, H as Height, w as Width
+            //So it will start croping from  Point(x,y) and end up point(width,height) of given image
+            //where ImageName is the Image path resides on the server http://localhost/Images Directory
+
+
 
             routes.Add(new Route("cropImages/{x}-{y}-{h}-{w}-{imgName}", new ImageCroperRoutHandler()));
             routes.MapRoute(
